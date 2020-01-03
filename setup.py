27b4b2437c2d3551ef8111a6e37a6ad373a00cb9
mattsaxon <saxonmatt@hotmail.com>
2020-01-03 17:19:40 +0000

    fixed some dependancies

--- setup.py
+++ setup.py
@@ -11,24 +11,24 @@ with open('README.rst') as readme_file:
 with open('HISTORY.rst') as history_file:
     history = history_file.read()
 
-requirements = ['Click>=7.0', 'click_log', 'websockets', 'pycryptodome', 'requests']
+requirements = ['Click>=7.0', 'click_log', 'pycryptodome', 'requests', 'zeroconf']
 setup_requirements = []
-test_requirements = ['pytest', 'tox', 'python-coveralls']
+test_requirements = ['pytest', 'tox', 'python-coveralls', 'flask', 'flake8']
 
 setup(
-    author="Andrew Beveridge",
-    author_email='andrew@beveridge.uk',
+    author="Matt Saxon",
+    author_email='saxonmatt@hotmail.com',
     classifiers=[
         'Development Status :: 2 - Pre-Alpha',
         'Intended Audience :: Developers',
         'License :: OSI Approved :: MIT License',
         'Natural Language :: English',
-        'Programming Language :: Python :: 3.5',
         'Programming Language :: Python :: 3.6',
         'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
     ],
-    description="Interface for Sonoff devices running original Itead "
-                "firmware, in LAN mode.",
+    description="Interface for Sonoff devices running R3+ Itead "
+                "firmware.",
     entry_points={
         'console_scripts': [
             'pysonofflan=pysonofflan.cli:cli',
@@ -38,13 +38,13 @@ setup(
     license="MIT license",
     long_description=readme + '\n\n' + history,
     include_package_data=True,
-    keywords='pysonofflan',
-    name='pysonofflan',
-    packages=find_packages(include=['pysonofflan']),
+    keywords='pysonofflanr3',
+    name='pysonofflanr3',
+    packages=find_packages(include=['pysonofflanr3']),
     setup_requires=setup_requirements,
     test_suite='tests',
     tests_require=test_requirements,
-    url='https://github.com/beveradb/pysonofflan',
-    version='0.3.0',
+    url='https://github.com/mattsaxon/pysonofflan',
+    version='3.0.0',
     zip_safe=False,
 )
